8edc075c7177b87f083b1a490dd43c8fb3ded5e0
Bherzherk <christianelectron1c4@gmail.com>
2022-08-12 19:28:47 -0400

    Update with identification account

--- Python_Tutorial.py
+++ Python_Tutorial.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import streamlit as st 
 
 # title
@@ -21,3 +22,32 @@ st.code("print(a := Mylist)")
 st.subheader("Output")
 st.success("true")
 st.success("[1,2,3]")
+=======
+import streamlit as st 
+
+# title
+st.title("Python tutorial: Tricks to make your life easier.")
+# header
+st.header("Simple but effective tips for every python lovers")
+# text
+st.text("Here you have 10 tips to use in python to make easier your data science path")
+
+#Tutorial
+#subheader
+# step 1 
+st.header("1. Walrus operator")
+st.text("The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.")
+st.subheader("Example:")
+st.code("Mylist = [1,2,3]")
+with st.echo():
+    if(l := len(Mylist) > 2):
+        print(l) #true
+    
+    print(a := Mylist) 
+    print(a) 
+
+
+st.subheader("Output")
+st.success("true")
+st.success("[1,2,3]")
+>>>>>>> f82f947... Updating app 08-12
--- mojix_bootcamp_first_app.py
+++ mojix_bootcamp_first_app.py
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 import streamlit as st 
 
 st.write("HELLO FROM MOJIX BOOTCAMP")
+=======
+import streamlit as st 
+
+st.write("HELLO FROM MOJIX BOOTCAMP")
+
+>>>>>>> f82f947... Updating app 08-12
