defe88206c3995799132b98f6212a482fbc28975
bherzherker <christianelectron1c4@gmail.com>
2022-08-11 20:34:13 -0400

    Add files via upload

---
+++ Python_Tutorial.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+# title
+st.title("Python tutorial: Tricks to make your life easier.")
+# header
+st.header("Simple but effective tips for every python lovers")
+# text
+st.text("Here you have 10 tips to use in python to make easier your data science path")
+
+#Tutorial
+#subheader
+# step 1 
+st.header("1. Walrus operator")
+st.text("The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.")
+st.code("")
